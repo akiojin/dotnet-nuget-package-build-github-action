32053aeb109852fb5e4784fd1063150c1f0574d1
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2022-03-03 21:06:09 +0900

    初期コミット

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+
+async function Run(): Promise<void> 
+{
+	try {
+		const configuration: string = core.getInput('configuration')
+		const apiKey: string = core.getInput('api-key')
+		const output: string = core.getInput('output') || process.env.RUNNER_TEMP || __dirname
+		const source: string = core.getInput('source')
+		const publish: boolean = core.getBooleanInput('publish')
+
+		if (apiKey === '') {
+			throw new Error('api-key is null')
+		}
+		if (source === '') {
+			throw new Error('source is null')
+		}
+
+		await exec.exec('dotnet', ['build', '--configuration', configuration, '--output', output])
+
+		if (!!publish) {
+			await exec.exec('dotnet', ['nuget', 'push', `"${output}/*.nupkg"`, '--source', source, '--api-key', apiKey])
+		}
+	} catch (ex: any) {
+		core.setFailed(ex.message);
+	}
+}
+
+Run()
